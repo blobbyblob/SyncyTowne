a2b5d49a5136121e3aed2d297cf064d4a546c35b
blobbyblob <31461220+blobbyblob@users.noreply.github.com>
2018-10-28 13:23:12 -0400

    Load from filesystem added; testing

--- Plugin2/Plugin2/Plugin/ProjectSync/StudioModel.module.lua
+++ Plugin2/Plugin2/Plugin/ProjectSync/StudioModel.module.lua
@@ -4,7 +4,8 @@ Represents all "saveable" objects using Lua tables.
 
 Properties:
 	Root: the Instance at the root of the model.
-	Objects: all objects which can be saved.
+	Objects: all objects which can be saved. These are entries of the following form:
+		{ Object =  <object>; Hash = "<hash>"; }
 
 Events:
 	Changed: fires when any object is added/removed, or a property is changed on any object.
@@ -22,13 +23,15 @@ Constructors:
 local Utils = require(script.Parent.Parent.Parent.Utils);
 local Debug = Utils.new("Log", "StudioModel: ", true);
 
-local function CompareSource(a, b)
-	return a.Source == b.Source;
+local function CompareHashes(a, b)
+	Debug("Comparing %0t to %0t", a, b);
+	Debug("Hash Types: %s, %s", type(a.Hash), type(b.Hash));
+	return a.Hash == b.Hash;
 end
 local SAVEABLE_TYPES = {
-	Script = CompareSource;
-	LocalScript = CompareSource;
-	ModuleScript = CompareSource;
+	Script = CompareHashes;
+	LocalScript = CompareHashes;
+	ModuleScript = CompareHashes;
 };
 
 local StudioModel = Utils.new("Class", "StudioModel");
@@ -56,27 +59,36 @@ function StudioModel:Compare(other)
 		end
 		return equivalents[obj];
 	end
+
+	--Map `other._Objects[i].Object` -> `other._Objects[i]` for all i.
+	local otherObjectsMap = {};
+	for i, v in pairs(other._Objects) do
+		otherObjectsMap[v.Object] = v;
+	end
+
 	local backConvert = {};
-	for v in pairs(self._Objects) do
-		local corresponding = GetEquivalent(v);
-		if corresponding and other._Objects[corresponding] then
-			backConvert[other._Objects[corresponding]] = v;
-			if corresponding.ClassName == v.ClassName then
-				if SAVEABLE_TYPES[v.ClassName](v, corresponding) then
-					table.insert(results, { A = v; B = corresponding; Status = "synced"; });
+	for i, v in pairs(self._Objects) do
+		local corresponding = GetEquivalent(v.Object);
+		if corresponding and otherObjectsMap[corresponding] then
+			backConvert[corresponding] = v;
+			if corresponding.ClassName == v.Object.ClassName then
+				local comparisonResult = SAVEABLE_TYPES[v.Object.ClassName](v, otherObjectsMap[corresponding]);
+				Debug("Comparing %s against %s: %s", v, otherObjectsMap[corresponding], comparisonResult);
+				if comparisonResult then
+					table.insert(results, { A = v; B = otherObjectsMap[corresponding]; Status = "synced"; });
 				else
-					table.insert(results, { A = v; B = corresponding; Status = "desynced"; });
+					table.insert(results, { A = v; B = otherObjectsMap[corresponding]; Status = "desynced"; });
 				end
 			else
-				table.insert(results, { A = v; B = corresponding; Status = "classMismatch"; });
+				table.insert(results, { A = v; B = otherObjectsMap[corresponding]; Status = "classMismatch"; });
 			end
 		else
 			table.insert(results, { A = v; Status = "aOnly"; });
 			Debug("Object %s in self, not other", v);
 		end
 	end
-	for v in pairs(other._Objects) do
-		if not backConvert[other._Objects[v]] then
+	for i, v in pairs(other._Objects) do
+		if not backConvert[v.Object] then
 			Debug("Object %s in other, not self", v);
 			table.insert(results, { B = v; Status = "bOnly"; });
 		end
@@ -125,12 +137,12 @@ function StudioModel:_HookUpConnections()
 	end
 	self._Cxns.DescendantAdded = self._Root.DescendantAdded:Connect(function(descendant)
 		if SAVEABLE_TYPES[descendant.ClassName] then
-			self._Objects[descendant] = true;
+			table.insert(self._Objects, { Object = descendant; Hash = tostring(string.len(descendant.Source)); });
 			ListenTo(descendant);
 		end
 	end);
 	for i, v in pairs(self._Objects) do
-		ListenTo(i);
+		ListenTo(v.Object);
 	end
 end
 
@@ -151,7 +163,7 @@ function StudioModel.fromInstance(root)
 	--Find all Scripts, ModuleScripts, or LocalScripts in root and add them to the tree.
 	local function recurse(node)
 		if SAVEABLE_TYPES[node.ClassName] then
-			self._Objects[node] = true;
+			table.insert(self._Objects, { Object = node; Hash = tostring(string.len(node.Source)); });
 		end
 		for i, v in pairs(node:GetChildren()) do
 			recurse(v);
@@ -162,15 +174,163 @@ function StudioModel.fromInstance(root)
 	return self;
 end
 
-function StudioModel.fromFilesystemModel(fm)
+local SUFFIXES = {
+	ModuleScript = ".module.lua";
+	LocalScript = ".client.lua";
+	Script = ".server.lua";
+};
+local function GetSuffix(filename)
+	for i, v in pairs(SUFFIXES) do
+		if filename:sub(-#v) == v then
+			return i, filename:sub(1, -#v - 1);
+		end
+	end
+end
+local SUFFIX_CONVERT_TO_OBJECT = {
+	ModuleScript = function() return Instance.new("ModuleScript"); end;
+	LocalScript = function() return Instance.new("LocalScript"); end;
+	Script = function() return Instance.new("Script"); end;
+};
 
+function StudioModel.fromFilesystemModel(fm)
+	local root = Instance.new("Folder");
+	local objects = {};
+	local function AddToRoot(path, name, obj)
+		Debug("AddToRoot(%s, %s, %s) called", path, name, obj);
+		local r = root;
+		local iterator = string.gmatch(path, "[^/]+");
+		iterator();
+		for dir in iterator do
+			if not r:FindFirstChild(dir) then
+				local f = Instance.new("Folder");
+				f.Name = dir;
+				f.Parent = r;
+			end
+			r = r:FindFirstChild(dir);
+		end
+
+		--If we will blow away a Folder, instead, replace it with our object.
+		if r:FindFirstChild(name) then
+			for i, v in pairs(r:FindFirstChild(name):GetChildren()) do
+				v.Parent = obj;
+			end
+			r:FindFirstChild(name).Parent = nil;
+		end
+		obj.Parent = r;
+	end
+	--Scan through the tree; anything that's a file should be created.
+	local function recurse(tree)
+		Debug("Scanning %s", tree.Name);
+		if tree.Type == "file" then
+			local class, root = GetSuffix(tree.Name);
+			local entry = { Object = SUFFIX_CONVERT_TO_OBJECT[GetSuffix(tree.Name)](); Hash = tree.Hash; };
+			entry.Object.Name = root;
+			AddToRoot(tree.Parent.FullPath, root, entry.Object)
+			table.insert(objects, entry);
+		elseif tree.Type == "folder" then
+			--Skip folders. We'll implicitly create them if we need to.
+			for i, v in pairs(tree.Children) do
+				recurse(v);
+			end
+		end
+	end
+	recurse(fm.Tree);
+
+	local self = StudioModel.new();
+	self._Root = root;
+	self._Objects = objects;
+	return self;
 end
 
+local function BuildFakeFilesystemModel(str)
+	local root = { Tree = { Name = "<root>"; Type = "folder"; FullPath = ""; Parent = nil; Children = {}; }; }
+	local stack = {root.Tree};
+	local lastIndentation;
+	for line in string.gmatch(str, "[^\n]+") do
+		local _, indentation = string.find(line, "^\t+");
+		if not lastIndentation then lastIndentation = indentation; end
+		if indentation ~= #line then
+			for i = indentation + 1, lastIndentation do
+				table.remove(stack);
+			end
+			local endsInSlash = string.find(line, "/$");
+			if endsInSlash then
+				--Create a folder.
+				lastIndentation = indentation + 1;
+				local name = string.sub(line, indentation + 1, endsInSlash - 1);
+				local folder = { Name = name; Type = "folder"; FullPath = stack[#stack].FullPath .. name .. "/"; Parent = stack[#stack]; Children = {}; };
+				stack[#stack].Children[name] = folder;
+				table.insert(stack, folder);
+			else
+				--Create a file.
+				lastIndentation = indentation;
+				local i = string.find(line, " [^ ]+$");
+				local name = string.sub(line, indentation + 1, i - 1);
+				local hash = string.sub(line, i + 1);
+				local file = { Name = name; Type = "file"; FullPath = stack[#stack].FullPath .. name; Parent = stack[#stack]; Hash = hash; };
+				stack[#stack].Children[name] = file;
+			end
+		end
+	end
+	return root;
+end
+
+local function PrintModel(model)
+	local rootName = model.Root:GetFullName();
+	local objectsSortedByFullName = {};
+	for i, v in pairs(model.Objects) do
+		table.insert(objectsSortedByFullName, {v.Object:GetFullName(); v});
+	end
+	table.sort(objectsSortedByFullName, function(a, b) return a[1] < b[1]; end);
+	for i, v in pairs(objectsSortedByFullName) do
+		local v = v[2];
+		Debug("%s (%s) - %s", string.sub(v.Object:GetFullName(), #rootName + 2), v.Object.ClassName, v.Hash);
+	end
+end
+
+local TEST_FOLDER = Utils.Misc.Create(
+	{	ClassName = "Folder";
+		{	ClassName = "Script";
+			Source = 'print("Hello world!")\n';
+		};
+		{	ClassName = "Script";
+			Name = "StudioOnly";
+			Source = 'print("Hello world!")\n';
+		};
+		{	ClassName = "Script";
+			Name = "Subfolder";
+			Source = 'print("Hello world!")\n';
+			{	ClassName = "ModuleScript";
+				Source = 'local module = {}\n\nreturn module\n';
+			};
+			{	ClassName = "LocalScript";
+				Source = 'print("Hello world!")\n'
+			};
+		};
+	}
+);
+
 function StudioModel.Test()
-	local m1 = StudioModel.fromInstance(game.ServerStorage.Folder);
-	Debug("%0t", m1);
-	local m2 = StudioModel.fromInstance(game.ServerStorage.Folder:Clone());
-	Debug("%0t", m1:Compare(m2));
+	local m1 = StudioModel.fromInstance(TEST_FOLDER);
+	local fm = BuildFakeFilesystemModel([[
+	Folder/
+		Script.server.lua 22
+		Subfolder.server.lua 22
+		Subfolder/
+			LocalScript.client.lua 23
+			ModuleScript.module.lua 33
+			FilesystemOnly.module.lua 33
+	]]);
+	local m2 = StudioModel.fromFilesystemModel(fm);
+	Debug("Model 1:");
+	PrintModel(m1);
+	Debug("Model 2:");
+	PrintModel(m2);
+	local comparison = m1:Compare(m2);
+	table.sort(comparison, function(a, b) return a.Status < b.Status; end);
+	for i, v in pairs(comparison) do
+		Debug("%s: %s (%s) - %s (%s)", v.Status, v.A and v.A.Object, v.A and v.A.Hash, v.B and v.B.Object, v.B and v.B.Hash);
+	end
 	m1:Destroy();
 	m2:Destroy();
 end
